54ede6a1385e02beabbde29bc09a9c4c1726751b
Au Wan Ying <77014359+wanyinggg@users.noreply.github.com>
2023-04-08 00:38:11 +0800

    Update mental.php

--- mental.php
+++ mental.php
@@ -1,6 +1,121 @@
 <!DOCTYPE html>
 
-<link rel="stylesheet" href="mental-style.css">
+<style>
+    body {
+      font-family: Arial, sans-serif;
+    }
+    
+    header {
+      background-color: #bbe5e1;
+      background-image: url('images/bg-image-1.png'),url('images/bg-image-2.png');
+      background-size: contain, contain;
+      background-repeat: no-repeat,no-repeat;
+      background-position: left, right;
+      position: relative;
+      padding: 100px;
+      font-family: Times;
+      font-size : 30px;
+      color: #27407E;
+    }
+
+    h1 {
+      text-align:center;
+      background:
+      linear-gradient(65deg , transparent 50%,rgba(3,78,136,0.7) 50%) left no-repeat, 
+      linear-gradient(0deg , rgba(3,78,136,0.7),rgba(3,78,136,0.7)) 30px 0 no-repeat;
+      background-size:30px 100%, 100% 100%;
+      width:400px;
+      height:60px;
+      padding:0 20px;
+      color:white;
+      margin:20px auto;
+      position:relative;
+    }
+
+    .subtitle {
+      width :80%;
+      margin: auto;
+      text-align: center;
+      padding-top: 100px;
+    }
+
+    .flex-container{
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: space-around;
+    }
+
+    .flex-item {
+      border-radius: 15px;
+      flex-basis: 31%;
+      background-color: #fff4df;
+      margin-bottom: 5%;
+      padding: 20px 12px;
+      box-sizing: border-box;
+      text-align:justify;
+    }
+
+    .container {
+      display: flex;
+      flex-wrap: wrap;
+      margin: auto;
+      justify-content: space-around;
+      padding: 15px;
+      align-items: center;
+    }
+
+    section {
+      justify-content: center;
+      text-align: center;
+      margin: 20px;
+      margin-bottom: 20px;;
+      width: 400px;
+      padding: 20px;
+      background-color: #f2f2f2;
+      border-radius: 10px;
+    }
+    
+    @media(max-width: 600px){
+      .flex-container{
+          flex-direction: column;
+      }
+    }
+
+    h2 {
+      margin-top: 0;
+      text-align: center;
+    }
+
+    .container-2 {
+      background-color: #fff4df;
+      text-align: justify;
+      width: fit-content;
+      padding: 50px;
+      margin: auto;
+      margin-top: 30px;
+      border-radius: 25px;
+    }
+    
+    button {
+      background-color: #27407E;
+      padding: 10px 20px;
+      border: none;
+      border-radius: 5px;
+      cursor: pointer;
+      margin: 0 auto;
+      margin-top: 10px;
+      color:white;
+    }
+    
+    button:hover {
+      background-color: #bbe5e1;
+      color: black;
+    }
+    
+    button:focus {
+      outline: none;
+    }
+  </style>
 
 <body>
     <?php 
