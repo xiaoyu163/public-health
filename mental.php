f648f3ee21349d7df232159234f7db38e3f332e2
Au Wan Ying <77014359+wanyinggg@users.noreply.github.com>
2023-04-08 00:29:50 +0800

    Update mental.php

--- mental.php
+++ mental.php
@@ -1,9 +1,119 @@
 <!DOCTYPE html>
+
+<link rel="stylesheet" href="mental-style.css">
+
 <body>
     <?php 
         include 'nav.php';
     ?>
 
-    <h2>Mental Health</h2>
+    <header>
+        <h1>Mental Health</h1>
+    </header>
+
+    <div class ="subtitle">
+        <h2>Know More About Mental Health Problems</h2>
+    </div>
+
+    <div class="flex-container">
+        <div class="flex-item">
+            <h2>Anxiety Disorders</h2>
+            <p>
+              <img src="anxiety.png" style="width:420px;height:260px;"><br>
+              For a person with an anxiety disorder, the anxiety does not go away and can get worse over time. The symptoms can interfere with daily activities such as job performance, schoolwork, and relationships.
+              <br><br>
+              <a href="https://www.mentalhealth.gov/what-to-look-for/anxiety-disorders"> Learn More</a>
+            </p>
+        </div>
+        <div class="flex-item">
+            <h2>Behavioral Disorders</h2>
+            <p>
+              <img src="behavioral.jpg" style="width:420px;height:260px;"><br>
+              Behavioral disorders involve a pattern of disruptive behaviors in children that last for at least 6 months and cause problems in school, at home and in social situations. 
+              <br><br>
+              <a href="https://www.mentalhealth.gov/what-to-look-for/behavioral-disorders"> Learn More</a>
+            </p>
+          </div>
+          <div class="flex-item">
+            <h2>Eating Disorders</h2>
+            <p>
+              <img src="eating.jpg" style="width:420px;height:260px;"><br>
+              Eating disorders involve extreme emotions, attitudes, and behaviors involving weight and food.
+              <br><br>
+              <a href="https://www.mentalhealth.gov/what-to-look-for/eating-disorders"> Learn More</a>
+            </p>
+          </div>  
+          <div class="flex-item">
+            <h2>Substance Use Disorders</h2>
+            <p>
+              <img src="substance.jpeg" style="width:420px;height:260px;"><br>
+              Substance use disorders can refer to substance use or substance dependence.          
+              <br><br>
+              <a href="https://www.mentalhealth.gov/what-to-look-for/mental-health-substance-use-disorders"> Learn More</a>
+            </p>
+          </div> 
+          <div class="flex-item">
+            <h2>Mood Disorders</h2>
+            <p>
+              <img src="mood.jpg" style="width:420px;height:260px;"><br>
+              If you have a mood disorder, your general emotional state or mood is distorted or inconsistent with your circumstances and interferes with your ability to function.
+              <br><br>
+              <a href="https://www.mentalhealth.gov/what-to-look-for/mood-disorders"> Learn More</a>  
+            </p>
+          </div> 
+          <div class="flex-item">
+            <h2>Personality Disorders</h2>
+            <p>
+              <img src="personality.jpg" style="width:420px;height:260px;"><br>
+              Personality disorders are conditions in which an individual differs significantly from an average person, in terms of how they think, perceive, feel or relate to others.           
+              <br><br>
+              <a href="https://www.mentalhealth.gov/what-to-look-for/personality-disorders">Learn More</a> 
+            </p>
+          </div> 
+          <div class="flex-item">
+            <h2>Psychotic Disorders</h2>
+            <p>
+              <img src="psychotic.jpeg" style="width:420px;height:260px;"><br>
+              People with a psychotic disorder may be convinced that someone is spying on them or following them, may hear voices or have the feeling that others are manipulating their thoughts.          
+              <br><br>
+              <a href="https://www.mentalhealth.gov/what-to-look-for/psychotic-disorders">Learn More</a>
+            </p>
+          </div> 
+          <div class="flex-item">
+            <h2>Self-Harm</h2>
+            <p>
+              <img src="self harm.jpg" style="width:420px;height:260px;"><br>
+              Self-harm refers to a person\'s harming their own body on purpose. 
+              <br><br>
+              <a href="https://www.mentalhealth.gov/what-to-look-for/self-harm"> Learn More</a>  
+            </p>
+          </div>
+          <div class="flex-item">
+            <h2>Suicidal Behavior</h2>
+            <p>
+              <img src="suicidal.jpg" style="width:420px;height:260px;"><br>
+              Suicide causes immeasurable pain, suffering, and loss to individuals, families, and communities nationwide. 
+              <br><br>
+              <a href="https://www.mentalhealth.gov/what-to-look-for/suicidal-behavior"> Learn More</a>
+            </p>
+          </div>
+          <div class="flex-item">
+            <h2>Post-Traumatic Stress Disorder</h2>
+            <p>
+              <img src="post traumatic.jpg" style="width:420px;height:260px;"><br>
+              You can get PTSD after living through or seeing a traumatic event, such as war, a hurricane, rape, physical abuse or a bad accident. 
+              <br><br>
+              <a href="https://www.mentalhealth.gov/what-to-look-for/post-traumatic-stress-disorder"> Learn More</a>
+            </p>
+          </div>
+        </div>
+
+        <div class="container">
+          <section>
+            <h2>Take a Stress Test</h2>
+            <p>Take this quiz to measure your stress level and get tips for reducing stress.</p>
+            <button onclick="location.href='stress-test.php'">Take Quiz</button>
+          </section>
+        </div>
 
 </body>
