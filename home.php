958c7a1adeb7a1f1b0e76a6e08dd79792b109f99
Lee Xiao Yu <u2005405@siswa.um.edu.my>
2023-04-08 22:54:22 +0800

    introduction

--- home.php
+++ home.php
@@ -11,18 +11,35 @@
         display: block;
         max-width:100%;
         max-height:100%;
-        opacity: 0.5;
+        opacity: 0.2;
     }
 
     .centered {
         position: absolute;
         top: 50%;
         left: 50%;
-        transform: translate(-50%, -50%);
+        transform: translate(-50%, 0%);
+        
+    }
+
+    .centered h2 {
+       
+        font-weight:bolder;
+    }
+
+    .centered h4 {
+       
+       font-weight:light;
+       text-align: justify;
+       
+   }
+
+    .centered p {
         font-style: italic;
         font-family:"Segoe Script";
-        font-size: larger;
-        font-weight: bolder;
+        font-size: medium;
+        font-weight: light;
+        text-align: justify;
     }
 
 
@@ -34,7 +51,24 @@
 
     <div class="container">
         <img src="images/health.png">
-        <p class="centered">Better Healthcare, Better Tomorrow.</p>
+        <div class="centered">
+            <h2>Public Health</h2>
+            <p>
+                Public health is a field that focuses on improving the health of populations by preventing diseases, promoting healthy behaviors, 
+                and ensuring access to healthcare services. It involves a range of activities, from conducting research on the causes and prevention 
+                of diseases to developing policies and programs that promote healthy living.
+            </p>
+            <h2>Healthcare Access</h2>
+            <p>
+                Healthcare access refers to the ability of individuals to obtain healthcare services when they need them. Access to healthcare services can be affected by a range of factors, including geographic location, income, insurance coverage, and cultural and linguistic barriers. 
+                Access to healthcare is important for maintaining good health, preventing and managing chronic diseases, and treating acute illnesses and injuries.
+            </p>
+
+            <h4><i>
+                The availability and accessibility of healthcare services can have a significant impact on the overall health and well-being of communities. Public health and healthcare access are closely related, 
+                as efforts to improve healthcare access often involve public health strategies, such as community-based interventions, health education, and policy advocacy.
+            </i></h4>
+        </div>
     </div>
 
     
